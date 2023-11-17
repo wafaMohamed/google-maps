539ed4f1987555077cc2eb9592aa75ed9b381d07
wafaMohamed <wafamohameddd@gmail.com>
2023-11-17 13:03:45 +0200

    commit

--- lib/core/constants/api_key/api_keys.dart
+++ lib/core/constants/api_key/api_keys.dart
@@ -1,4 +1,4 @@
 class ApiKeys {
-  static const String apiKeyGoogleMapSdkAndroid = 'AIzaSyBWqo2CWwYRLuPtvAXS7v5bOawcN2S3-iY';
-  static const String apiKeyPlaces = 'AIzaSyClCe0durrHakW2pRvAMv46UPCZFVhkXHU';
+  static const String apiKeyGoogleMapSdkAndroid = '';
+  static const String apiKeyPlaces = '';
 }
